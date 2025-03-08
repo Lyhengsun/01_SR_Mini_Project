84406bef3aea27a6c4c6e1d41990548e2c047831
Lyhengsun <101932930+Lyhengsun@users.noreply.github.com>
2025-03-08 12:01:39 +0700

    added input loop regex in ProductView

--- src/main/java/org/example/ProductController.java
+++ src/main/java/org/example/ProductController.java
@@ -1,14 +1,19 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class ProductController {
     private final ProductView pv;
     private final ProductModelImplement pmi;
+    private ArrayList<ProductModel> unsavedWrite = new ArrayList<>();
     ProductController(ProductView pv, ProductModelImplement pmi) {
         this.pv = pv;
         this.pmi = pmi;
     }
-
     public void printPaginationView() {
         System.out.println(pv.getProductPage(pmi.getAllProducts()));
+    }
+    public void writeProducts() {
+
     }
 }
